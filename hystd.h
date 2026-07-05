6234ae2f07d65f2fe203572f19e88cbf6e5e58b0
hyunxuuu <h3314829906@163.com>
2026-07-05 15:10:22 +0800

    feat: Add marco REG_BITS and REG_MASK.

--- hystd.h
+++ hystd.h
@@ -3,6 +3,9 @@
 
 #include <stdint.h>
 
+#define REG_BITS        (sizeof(reg_u) * 8)
+#define REG_MASK        (sizeof(reg_u) * 8 - 1)
+
 typedef uint32_t        reg_u;
 typedef int32_t         reg_i;
 typedef float           reg_f;
